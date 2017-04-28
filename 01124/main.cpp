2895ba663f245a95acdbc080fc6c66435962da47
Gi Wah Davalos <giwirodavalos@gmail.com>
2017-04-28 00:34:10 -0500

    se desarrollo lo mismo solo que en c++

--- 01124/main.cpp
+++ 01124/main.cpp
@@ -1,13 +1,12 @@
-#include <cstdio>
-#include <cstring>
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
-    char line[1024];
-    while(fgets(line, 1024, stdin)){
-        int len = strlen(line);
-        for (int i = 0; i < len; i++) {
-            printf("%c", line[i]);
-        }
+    string line;
+    while(getline(cin, line)){
+        cout << line << endl;
     }
     return 0;
 }
