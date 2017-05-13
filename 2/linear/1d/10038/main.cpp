a15ee393abab0fb995e06cc30bf728404f1eb114
redes <giwirodavalos@gmail.com>
2017-05-13 09:12:14 -0500

    se termino jolly jumpers

--- 2/linear/1d/10038/main.cpp
+++ 2/linear/1d/10038/main.cpp
@@ -1,54 +1,50 @@
 #include <iostream>
 #include <cmath>
+#include <cstring>
 
 using namespace std;
 
 int main() {
     int times, prev, num, seq = -1;
     bool jumper;
+    bool check[3000];
     while(cin >> times) {
         jumper = true;
+	memset(check, false, sizeof check);
         seq = -1;
-        cout << "--------------------" << endl;
-        cout << "times: " << times << endl;
+        // cout << "--------------------" << endl;
+        // cout << "times: " << times << endl;
         cin >> prev;
         if (times == 1) {
-            cout << "Scape 1 prev: " << prev << endl;
-            cout << "Jolly" << endl;
-            continue;
-        }
-        if (times == 2) {
-            cin >> num;
-            cout << "Scape 2 prev: " << prev << endl;
-            cout << "Scape 2 num: " << num << endl;
+            // cout << "Scape 1 prev: " << prev << endl;
             cout << "Jolly" << endl;
             continue;
         }
 
         for (int i = 0; i < times - 1; i++) {
             cin >> num;
-            cout << "--" << endl;
-            cout << "prev: " << prev << endl;
-            cout << "num: " << num << endl;
+            // cout << "--" << endl;
+            // cout << "prev: " << prev << endl;
+            // cout << "num: " << num << endl;
             int temp = abs(prev - num);
-            cout << "seq: " << temp << endl;
-            if (seq != -1) {
-                if (seq - 1 != temp) {
-                    jumper = false;
-                } 
-            }else {
-                seq = temp;
-            }
+            // cout << "seq: " << temp << endl;
             seq = temp;
+	    check[seq] = true;
             // cout << "seq: " << seq << endl;
             prev = num;
         }
+
+	for (int j = 1; j < times; j++) {
+		if (!check[j]) {
+			jumper = false;
+		}
+	}
         if (jumper) {
             cout << "Jolly" << endl;
         }else {
             cout << "Not jolly" << endl;
         }
-        cout << "--------------------" << endl;
+        // cout << "--------------------" << endl;
     }
     return 0;
 }
